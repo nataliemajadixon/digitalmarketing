153dddd66c7b5e39191a990a83b3891830131378
natolay <ndixon@seattleu.edu>
2023-05-18 13:05:29 +0200

    created navbar and routes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,30 +2,25 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 import './Navbar.css'
 
 const Navbars = () => {
     return (
-        <Navbar bg="light" expand="lg" >
+        <Navbar expand="lg" sticky="top" style={{backgroundColor: 'rgb(255, 252, 252)'}} >
         <Container fluid>
-          <Navbar.Brand href="/">Nats</Navbar.Brand>
+          <Navbar.Brand href="/"> <img src={require('../../assets/natslogo.png')} alt='businesslogo' style={{height: '60px', paddingLeft: '80px'}} /></Navbar.Brand>
+         
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+            <Nav className="ms-auto navigation">
+              <Nav.Link href="/digital" className="px-3" >Digital</Nav.Link>
+              <Nav.Link href="/services" className="px-3" >Services</Nav.Link>
+              <Nav.Link href="/about" className="px-3">About Us</Nav.Link>
+              <Nav.Link href="/career" className="px-3">Career</Nav.Link>
+              <Nav.Link href="/contact" className="px-3">Contact</Nav.Link>
+
+            
             </Nav>
           </Navbar.Collapse>
         </Container>
