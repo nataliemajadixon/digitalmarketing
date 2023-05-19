251d813f4072914c9af14969720f5ec2c8572023
natolay <ndixon@seattleu.edu>
2023-05-19 16:19:48 +0200

    added sections to homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,6 +18,33 @@ const Home = () => {
         </div>
         </div>
     </section>
+    <section>
+        <div className="servicecon">
+<div className='scontainers'>
+    <div className='scontainershead'>UX design</div>
+    <div className='scontainersmain'>
+UX design is about creating good user experiences. The goal is to develop products and services that simplify and improve the experience for people.
+</div>
+
+</div>
+<div className='scontainers'>
+    <div className='scontainershead'>App development</div>
+    <div className='scontainersmain'>Modern smartphones possess enormous power. With an app, we can make full use of this.</div>
+
+</div>
+<div className='scontainers'>
+    <div className='scontainershead'> Augmented Reality</div>
+    <div className='scontainersmain'>Using the latest AR application development frameworks, we can help you and your users take the 
+leap into the future of computing, today.</div>
+</div>
+<div className='scontainers'>
+<div className='scontainershead'>Animated movies</div>
+<div className='scontainersmain'>
+When you need to explain a product, service or complex flows in moving form.
+</div>
+</div>
+        </div>
+    </section>
     </>
     )
 }
