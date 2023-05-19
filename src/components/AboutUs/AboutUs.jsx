ae55e884368f28e1666031c628a731f3f120dfab
natolay <ndixon@seattleu.edu>
2023-05-19 15:24:20 +0200

    updated home page

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -5,6 +5,13 @@ import './AboutUs.css'
 const About = () => {
     return(
 <>
+<div className="abouttopdiv">
+<img src={require('../../assets/footballfield.jpeg')} alt='footballfield' style={{ width: "100vw", height:'100vh'}} />
+
+</div>
+
+
+
 <h1> layout for this page</h1>
 <h4>Have a soccer ball cursor with footballfield backrground</h4>
 <h5> Have our goals for the company</h5>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,18 @@ const Home = () => {
     return (
 
     <>
-    <h1>test</h1>
+    <section className="topsection">
+<img src={require('../../assets/vrhome.jpeg')} alt='vrheadset' style={{ filter: 'brightness(70%)', width: '100vw', height: '100vh', objectFit: 'cover' }}/>
+        <div className='toptext'>
+            <div className="bigtop">
+            Welcome to Nats -digital agency
+            </div>
+            {'\n'} 
+            <div className="smalltop">
+        We will join you in reaching your full potential
+        </div>
+        </div>
+    </section>
     </>
     )
 }
