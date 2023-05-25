c3bc1a45c6dfb614ee1ee7b53ad8b6f758b925c2
natolay <ndixon@seattleu.edu>
2023-05-25 13:42:01 +0200

    new update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,10 +18,10 @@ const Home = () => {
         </div>
         </div>
     </section>
-    <section>
+    <section className="servicehome">
         <div className="servicecon">
 <div className='scontainers'>
-
+<div className="iconhome"><i className="bi bi-person-fill"></i></div>
     <div className='scontainershead'>UX design</div>
 
     <div className='scontainersmain'>
@@ -30,69 +30,82 @@ UX design is about creating good user experiences. The goal is to develop produc
 
 </div>
 <div className='scontainers'>
+    <div className="iconhome"><i className="bi bi-phone"></i></div>
     <div className='scontainershead'>App development</div>
     <div className='scontainersmain'>Modern smartphones possess enormous power. With an app, we can make full use of this.</div>
 
 </div>
 <div className='scontainers'>
+    <div className="iconhome"><i className="bi bi-boxes"></i></div>
     <div className='scontainershead'> Augmented Reality</div>
     <div className='scontainersmain'>Using the latest AR application development frameworks, we can help you and your users take the 
 leap into the future of computing, today.</div>
 </div>
 <div className='scontainers'>
+    <div className="iconhome"><i className="bi bi-chat-text"></i></div>
 <div className='scontainershead'>Animated movies</div>
 <div className='scontainersmain'>
 When you need to explain a product, service or complex flows in moving form.
 </div>
 </div>
         </div>
-        <div className="sbottom">See more of our services or contact us via email:hello@hoys.se</div>
+
+        {/* <div className="sbottom">See more of our services or contact us via email:hello@hoys.se</div> */}
     </section>
-<section >
+<section className="homeinfo" >
     <div className="nats">
+        <div className="natsbox">
         <div className="homeheaders">THIS IS NATS</div>
         <div className="homebody">A digital agency in Sweden with talent for technology, design and film.
              We are project managers, strategists, developers, designers, creators, directors 
              and motion artists.</div>
+             </div>
     </div>
 </section>
 
 <section>
     <div className="motivation">
+        <div className='motivationbox'>
         <div className="homeheaders">OUR MOTIVATION</div>
         <div className="homebody">
         We are tasked with creating valueable work through our passion. 
         The goal is to simplify, improve and design. 
         We make life easier for everyone.
         </div>
+        </div>
     </div>
 </section>
 
 <section>
     <div className="work">
+        <div className='workbox'>
         <div className="homeheaders">HOW WE WORK</div>
         <div className="homebody">
         We work closely and in an integrated manner with our clients. Puts us into 
         the business, brainstorms 
         strong creative ideas, knocks the code and measures the effect. All under one roof.
         </div>
+        </div>
     </div>
 </section>
 <section>
     <div className="flexhome">
         <div className="flexboxlayout1">
+        <a href="/services">
        
            <div className="flexboxlayout1text">
             <div>Services</div>
-            <div>Read More</div>
+            <div className="readmore">Read More</div>
             </div>
+            </a>
         </div>
         <div className="flexboxlayout2">
-       
-       
+        <a href="/about">
+       <div className="flexboxlayout2text"> 
         <div>About Nats</div>
-        <div>Read More</div>
-     
+        <div className="readmore">Read More</div>
+        </div>
+        </a>
         </div>
     </div>
 </section>
@@ -118,7 +131,7 @@ When you need to explain a product, service or complex flows in moving form.
         <p className='bottomheaders'>SOCIAL</p>
         <p>
             <a style={{paddingRight: '10px'}} href="https://main.dkibz9e78ls53.amplifyapp.com/" i className="bi bi-instagram bottomlinks"> </a>
-         <a href="https://github.com/nataliemajadixon" i className="bi bi-facebook bottomlinks"> </a>
+         <a style={{paddingRight: '10px'}} href="https://github.com/nataliemajadixon" i className="bi bi-facebook bottomlinks"> </a>
          <a href="https://www.linkedin.com/in/nataliedixon369/" i className="bi bi-linkedin bottomlinks"> </a>
          </p>
         
